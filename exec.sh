fd37ad2b0a93e4744d009d753c733a1714039f6f
Michael <87473277+MichaelB11221@users.noreply.github.com>
2026-09-02 19:17:41 +0200

    Update exec.sh

--- exec.sh
+++ exec.sh
@@ -1,2 +1,11 @@
-# Oneliner
-bash <(curl -fsSL https://raw.githubusercontent.com/MichaelB11221/dotfiles/main/install.sh)
+# Curl & Bash
+# bash <(curl -fsSL https://raw.githubusercontent.com/MichaelB11221/dotfiles/main/install.sh)
+
+# Curl & ZSH 
+# zsh <(curl -fsSL https://raw.githubusercontent.com/MichaelB11221/dotfiles/main/install.sh)
+
+# Git pull repo 
+# git pull https://github.com/MichaelB11221/dotfiles.git
+
+# GH repo Clone
+# gh repo clone https://github.com/MichaelB11221/dotfiles.git
